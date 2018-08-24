6dc241ad15f9102b6458d2d3fea2d295686b0ade
bolin <hjt32@126.com>
2018-08-24 13:10:52 +0800

    Signed-off-by: bolin <hjt32@126.com>

---
+++ download.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'lib/config.php';
+include_once 'header.php';
+?>
+
+<div class="container">
+    <div class="section"> 
+        <!--   Icon Section   -->
+        <div class="row">
+            <div class="row marketing">
+                <h2 class="sub-header">软件下载</h2>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>###</th>
+                            <th>版本</th>
+                            <th>链接</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Windows</td>
+                                <td><a href="/download/ssr-win.7z">下载</a></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Mac</td>
+                                <td><a href="/download/ssr-mac.dmg">下载</a></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Android</td>
+                                <td><a href="/download/ssr-android.apk">下载</a></td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Win游戏专用</td>
+                                <td><a href="/download/SSTap.7z">下载</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>  
+</div>
+<?php  include_once 'ana.php';
+include_once 'footer.php';?>
--- lib/config-simple.php
+++ lib/config-simple.php
@@ -8,15 +8,15 @@
 
 //定义流量
 $tokb = 1024;
-$tomb = 1024*1024;
-$togb = $tomb*1024;
+$tomb = 1024 * 1024;
+$togb = $tomb * 1024;
 //Define DB Connection  数据库信息
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PWD','password');
-define('DB_DBNAME','db');
-define('DB_CHARSET','utf8');
-define('DB_TYPE','mysql'); 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'ssr');
+define('DB_PWD', 'ssr');
+define('DB_DBNAME', 'ssr');
+define('DB_CHARSET', 'utf8');
+define('DB_TYPE', 'mysql');
 /*
  * 下面的东西根据需求修改
  */
@@ -24,20 +24,24 @@ define('DB_TYPE','mysql');
 //define Plan
 //注册用户的初始化流量
 //默认5GiB
-$a_transfer = $togb*5;
+$a_transfer = $togb * 5;
 
 //签到设置 签到活的的最低最高流量,单位MB
-$check_min = 1;
-$check_max = 100;
+$check_min = 100;
+$check_max = 500;
 
+$check_invite = 100;
+
+$port_min = 10000;
+$port_max = 65535;
 //name
-$site_name = "ss-panel";
-$site_url  = "https://panel.com/";
+$site_name = "ssr";
+$site_url = "http://ssr.***.com/";
 /**
  * 站点盐值，用于加密密码
  * 第一次安装请修改此值，安装后请勿修改！！否则会使所有密码失效，仅限加密方式不为1的时候有效
  */
-$salt = "ss-panel";
+$salt = "*****************";
 /**
  * 密码加密方式，注意： 2.4以前的版本，请修改加密方式为「1」，否则会使密码失效！
  * 更多说明见wiki https://github.com/orvice/ss-panel/wiki/Install-Guide-zh_cn
@@ -51,40 +55,28 @@ $pwd_mode = 1;
 //都设置为0用户就不能邀请
 $user_invite_min = '1';
 $user_invite_max = '1';
+//推荐用户获得流量M
+$user_invite_get = 1024;
 
+//允许注册的邮件后缀
+$mail_allow_domain = ['gmail.com', '163.com', 'qq.com', 'live.cn', 'live.com', 'sohu.com', 'yeah.net', 'vip.qq.com', 'foxmail.com', 'outlook.com', 'hotmail.com', 'sina.com', '126.com', 'icloud.com'];
 
-//
-//选择邮件服务
-// smtp未完成，现在只能用mailgun
-//mail-gun
-//mail-smtp
-$Selectmailservice = "mail-gun";
 //邮件发件人
-$sender = "xxx@xxx.xx";
+$mail_smtp_Sender = "ssr小组";
 
-//mail-gun
-// Get your key from https://mailgun.com
-$mailgun_key = "";
-$mailgun_domain = "";
-
-
-//
 //mail-smtp
-// smtp发件方式暂时无法使用
-//设置smtp服务器连接方式:  
-//加密连接(ssl) = "1"
-//普通连接 = "0"
-$mail_smtp_Connection = "1";
+$mail_smtp_Domain = "*****.com";
+//smtp发件方式暂时无法使用
+//设置smtp服务器连接方式:
+$mail_smtp_Secure = "ssl";
 //smtp服务器端口 25 , 465 ...
 $mail_smtp_Port = 465;
 //smtp服务器
-$mail_smtp_Server = "smtp.gmail.com";
+$mail_smtp_Server = "smtp.qq.com";
 //邮件帐号
-$mail_smtp_Account = "xxxx@gmail.com";
+$mail_smtp_Account = "ssuper@*****.com";
 //邮件密码
-$mail_smtp_password = "密码";
-
-
+$mail_smtp_password = "******************";
 
 //
 require_once 'do.php';
